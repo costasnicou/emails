b5d594f5fc188defdae6f685c58790c5144f6051
Kostas Nikou <nicoucostas@gmail.com>
2026-04-04 09:48:01 +0300

    progress

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -12,6 +12,29 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
+  const composeSubmit = document.querySelector('.compose-submit');
+  composeSubmit.addEventListener('click',function(e){
+    // e.preventDefault();
+    const recipientsValue = document.querySelector('#compose-recipients').value;
+    const subjectValue =  document.querySelector('#compose-subject').value;
+    const composeBodyValue = document.querySelector('#compose-body').value;
+
+      fetch('/emails', {
+        method: 'POST',
+        body: JSON.stringify({
+            recipients: recipientsValue,
+            subject: subjectValue,
+            body: composeBodyValue,
+        })
+      })
+      .then(response => response.json())
+      .then(result => {
+          // Print result
+          console.log(result);
+    
+      });
+  });
+
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -22,7 +45,7 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 
-
+ 
 }
 
 function load_mailbox(mailbox) {
@@ -30,51 +53,123 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
+  document.querySelector('#single-email').style.display = 'none';
+  document.querySelector('#single-email').style.display = 'none';
+  
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `
     <h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>
-    <div class="emails-content"></div>
+    <div class="emails-wraper"></div>
   `;
   
-  // console.log(base_url);
-  // load the targeted mailbox
-    fetch(`/emails/${mailbox}`)
-    .then(response => response.json())
-    .then(emails => {
-        // Print emails
-        let emailsContent = document.querySelector('.emails-content');
-        emails.forEach(email=>{
-          let html = `<p>${email.id}: Subject: ${email.subject} from ${email.sender}</p>`;
-          emailsContent.insertAdjacentHTML('afterbegin',html);
-        });
-        console.log(emails);
 
-        // ... do something else with emails ...
-    }).catch( error =>{
-      console.log(error);
-    });
+  // load the targeted mailbox
+   fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      // emails.sort((a,b)=> a.id - b.id);
+      let emailsWraper = document.querySelector('.emails-wraper');
+      emails.forEach(email=>{
+        let emailstatus ="";
+
+        if(email.read){
+          emailstatus="read";
+        }
+        else{
+          emailstatus="unread";
+        }
+
+
+        let html = `<div id="${email.id}" class="email ${emailstatus}"><p><strong>${email.sender}</strong> Subject: ${email.subject} <span class="timestamp">${email.timestamp}</span></p></div>`;
+        emailsWraper.insertAdjacentHTML('beforeend',html);
+        
+      });
+      console.log(emails);
+
+      // ... do something else with emails ...
+  }).catch( error =>{
+    console.log(error);
+  });
+
+  
+  load_email();
+
 }
 
-const composeSubmit = document.querySelector('.compose-submit');
-composeSubmit.addEventListener('click',function(){
 
-  const recipientsValue = document.querySelector('#compose-recipients').value;
-  const subjectValue =  document.querySelector('#compose-subject').value;
-  const composeBodyValue = document.querySelector('#compose-body').value;
- 
-    fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-          recipients: recipientsValue,
-          subject: subjectValue,
-          body: composeBodyValue,
+function load_email(){
+
+  // email selection
+  const emailsWraper = document.querySelector('.emails-wraper');
+
+  emailsWraper.addEventListener('click',(event)=>{
+    const closestChild = event.target.closest('p');
+    const childdiv = event.target.closest('.email');
+    if(closestChild || childdiv) {
+      // console.log("Email clicked");
+        const emailID = childdiv.getAttribute('id');
+        // console.log(emailID);
+      
+
+      fetch(`/emails/${emailID}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
       })
-    })
-    .then(response => response.json())
-    .then(result => {
-        // Print result
-        console.log(result);
-   
-    });
-});
+
+      fetch(`/emails/${emailID}`)
+      .then(response => response.json())
+      .then(email => {
+          // Print email
+          // Show the mail and hide other views
+          document.querySelector('#emails-view').style.display = 'none';
+          document.querySelector('#compose-view').style.display = 'none';
+          document.querySelector('#single-email').style.display = 'block';
+
+        
+          const singleEmailView = document.querySelector('#single-email');
+          const html =`
+            
+            <div id="${email.id}" class="email-wraper">
+              <h2><strong>Subject: </strong>${email.subject}</h2>
+              <p><strong>Sender: </strong>${email.sender}</p>
+              <p><strong>Recipients: </strong>${email.recipients}</p>
+              <p><strong>Body: </strong>${email.body}</p>
+              <p><strong>Timestamp: </strong>${email.timestamp}</p>
+
+            </div>
+          `;
+
+          const singleEmails = document.querySelectorAll('.email-wraper')
+          // if (!singleEmailID == email.id){
+
+          // }
+          singleEmails.forEach(singleemail => {
+            singleemail.style.display = "none";
+          });
+
+        
+          singleEmailView.insertAdjacentHTML("afterbegin",html);
+
+          const singleEmailID = document.querySelector(`#${email.id}`);
+          if (singleEmailID.getAttribute('id')==email.id){
+            singleEmailID.style.display="block";
+          }
+      
+          // ... do something else with email ...
+        });
+    }
+
+
+    else{
+      console.log("not working");
+    }
+  });
+
+ 
+}
+
+
+
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/cnicou/Desktop/CS50/emails/venv
+set -gx VIRTUAL_ENV '/home/cnicou/Desktop/CS50 Web Development with Python and Javascript/emails/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
